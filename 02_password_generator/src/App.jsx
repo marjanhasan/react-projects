c9705cb9651fc6cf787dc5257342f4c26e9f8831
marjanhasan <moonmarjanhasan@gmail.com>
2024-09-18 20:42:48 +0600

    some revision done at password generator projects

--- 02_password_generator/src/App.jsx
+++ 02_password_generator/src/App.jsx
@@ -33,11 +33,11 @@ function App() {
     passwordGenerator();
   }, [length, numberAllowed, charAllowed, passwordGenerator]);
   return (
-    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
-      <h1 className="text-white text-xl font-bold text-center my-3">
+    <div className="shadow-md rounded-lg px-2 md:px-16 py-3 my-8 bg-gray-800 text-orange-500">
+      <h1 className="text-white text-xl font-bold text-center my-7">
         Password Generator
       </h1>
-      <div className="flex shadow rounded-lg overflow-hidden mb-4">
+      <div className="flex shadow rounded-lg overflow-hidden mb-8">
         <input
           type="text"
           value={password}
@@ -53,7 +53,7 @@ function App() {
           copy
         </button>
       </div>
-      <div className="flex text-sm gap-x-2">
+      <div className="flex flex-wrap text-sm gap-x-2 mb-4">
         <div className="flex items-center gap-x-1">
           <input
             type="range"
