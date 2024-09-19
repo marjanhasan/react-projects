4fddeb1cf8ab401cfc326cad39819d5b91f9c02a
marjanhasan <moonmarjanhasan@gmail.com>
2024-09-19 23:30:49 +0600

    react router revision done

---
+++ 04_react_router/src/ErrorElements.jsx
@@ -0,0 +1,9 @@
+const ErrorElements = () => {
+  return (
+    <div>
+      <h1>404 not found</h1>
+    </div>
+  );
+};
+
+export default ErrorElements;
---
+++ 04_react_router/src/main.jsx
@@ -0,0 +1,31 @@
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Layout from "./Layout";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
+import User from "./components/User/User";
+import Github, { githubInfoLoader } from "./components/Github/Github";
+import ErrorElements from "./ErrorElements";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />} errorElement={<ErrorElements />}>
+      <Route path="" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="user/:userid" element={<User />} />
+      <Route loader={githubInfoLoader} path="github" element={<Github />} />
+    </Route>
+  )
+);
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
